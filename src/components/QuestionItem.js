9aee80240afa913141d7028af18daa85b5d7a2b0
Brandon <brandonslee3@gmail.com>
2024-04-09 16:24:34 -0400

    complete lab

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function QuestionItem({ question }) {
+function QuestionItem({ question, setQuestions }) {
   const { id, prompt, answers, correctIndex } = question;
 
   const options = answers.map((answer, index) => (
@@ -9,15 +9,34 @@ function QuestionItem({ question }) {
     </option>
   ));
 
+  function deleteQuestion(){
+    fetch(`http://localhost:4000/questions/${id}`,{
+      method: 'DELETE'
+    })
+    setQuestions(question => question.filter(element => element.id != id))
+  }
+
+  function changeAnswer(event){
+    console.log(event.target.value)
+    fetch(`http://localhost:4000/questions/${id}`,{
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify({correctIndex: event.target.value})
+    })
+  }
+
   return (
     <li>
       <h4>Question {id}</h4>
       <h5>Prompt: {prompt}</h5>
       <label>
         Correct Answer:
-        <select defaultValue={correctIndex}>{options}</select>
+        <select defaultValue={correctIndex} onChange={changeAnswer}>{options}</select>
       </label>
-      <button>Delete Question</button>
+      <button onClick={deleteQuestion}>Delete Question</button>
     </li>
   );
 }
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,14 @@
 import React from "react";
+import QuestionItem from "./QuestionItem";
 
-function QuestionList() {
+function QuestionList({questions, setQuestions}) {
+
+  const displayQuestions = questions.map(question => <QuestionItem question={question} key={question.id} setQuestions={setQuestions}/>)
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{displayQuestions}</ul>
     </section>
   );
 }
